bd291b0f451e1291f25538d1c48fc00dc82d394e
Francisco <56081472+REPTaiLE@users.noreply.github.com>
2024-01-29 22:09:47 -0300

    Actualización

--- pagina-inicio-admin/landingPageAdmin.js
+++ pagina-inicio-admin/landingPageAdmin.js
@@ -1,21 +1,19 @@
 // Menú Hamburguesa
 
-// const line1__bars = document.querySelector('.line1__bars-menu');
-// const line2__bars = document.querySelector('.line2__bars-menu');
-// const line3__bars = document.querySelector('.line3__bars-menu');
 const selectNav = document.querySelector('.nav');
 const selectButton = document.querySelector('.close-button');
+const subMenu = document.querySelector('.deployment');
 
 document.querySelector('#bars').addEventListener('click', () => {
 	setTimeout(function() {
         selectNav.classList.toggle('visiblenav');
     }, 160);
-    // line1__bars.classList.toggle('activeline1__bars-menu');
-    // line2__bars.classList.toggle('activeline2__bars-menu');
-    // line3__bars.classList.toggle('activeline3__bars-menu');
-    // selectNav.classList.toggle('visiblenav');
 });
 
 document.querySelector('.close-button').addEventListener('click', () => {
     selectNav.classList.remove('visiblenav');
+});
+
+document.querySelector('.sub-menu-item').addEventListener('mouseover', () => {
+    subMenu.classList.add('deployvisible');
 });
