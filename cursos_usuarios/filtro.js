6d9e50f634ca7408f9ab13042d18c4a1abe17878
Ariel Cruz <acruzo2025@gmail.com>
2024-01-31 17:28:04 -0300

    filtros v2

--- cursos_usuarios/filtro.js
+++ cursos_usuarios/filtro.js
@@ -1,22 +1,46 @@
-function filtro() {
-    // Accede al texto de entrada y a varios elementos del DOM
-    let valor = document.getElementById("searchInput").value.toUpperCase();
-    let cartas = document.getElementById("card");
-    let filas = cartas.getElementsByTagName("h5");
-  
-    // Iteramos el arreglo de filas
-    for(let i = 0; i < filas.length; i++){
-      // Por cada fila se obtiene la referencia a la columna lenguaje
-      let columnaLenguaje = filas[i].getElementsByTagName("h5");
-      // Se extrae el texto de la columna lenguaje
-      let lenguaje = columnaLenguaje.textContent;
-      // Se muestra u oculta la fila si la entrada de texto
-      // coincide con el texto de la columna lenguaje
-      filas[i].style.display = lenguaje.toUpperCase().indexOf(valor) > -1 ? "" : "none";
-    }
-  }
-  
-  // Se agrega un escuchador de eventos a la entrada de texto
-  // para activar la función de filtrado cada vez que el usuario
-  // ingresa texto en la entrada
-  document.getElementById("searchInput").addEventListener("keyup", filtro);
+//Busqueda Filtro
+document.getElementById('searchInput').addEventListener('input', function () {
+  const filter = this.value.toLowerCase();
+  const courseList = document.getElementsByClassName('course-link');
+
+  // Itera sobre los elementos y muestra/oculta según la coincidencia con el filtro
+  Array.from(courseList).forEach(function (course) {
+      const titleText = course.innerText.toLowerCase();
+      const card = course.closest('.card');
+
+      if (titleText.includes(filter)) {
+          card.classList.remove('hidden');
+      } else {
+          card.classList.add('hidden');
+      }
+  });
+});
+//Boton filtro
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+const appendAlert = (message, type) => {
+  const wrapper = document.createElement('div')
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '</div>'
+  ].join('')
+
+  alertPlaceholder.append(wrapper)
+}
+
+const alertTrigger = document.getElementById('liveAlertBtn')
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', () => {
+    appendAlert('Nice, you triggered this alert message!', 'success')
+  })
+}
+
+//FILTROOO
+document.getElementById('filterButton').addEventListener('click', function () {
+  const filterSection = document.getElementById('filterSection');
+
+  // Si la sección de filtros está visible, la oculta; de lo contrario, la muestra
+  filterSection.style.display = filterSection.style.display === 'none' ? 'block' : 'none';
+});
+
