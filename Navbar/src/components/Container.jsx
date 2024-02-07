990640cfff80970a1ee9c2d2c0daa495058db515
Francisco <56081472+REPTaiLE@users.noreply.github.com>
2024-02-07 18:57:10 -0300

    Navbar React | Sin funcionalidad, Componente padre: Container.jsx

---
+++ Navbar/src/components/Container.jsx
@@ -0,0 +1,12 @@
+import '../styles/Container.scss';
+import Header from './Header';
+
+const Container = () => {
+    return (
+        <div className="grid-container">
+            <Header />
+        </div>
+    );
+}
+
+export default Container;
