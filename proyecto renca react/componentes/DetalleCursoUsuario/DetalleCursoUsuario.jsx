a89979dcb68f8a824e991bc562a15c54377f34ff
Daniela Morales <dpmorales1@uc.cl>
2024-02-16 10:22:05 -0300

    16_febrero

--- proyecto renca react/componentes/DetalleCursoUsuario/DetalleCursoUsuario.jsx
+++ proyecto renca react/componentes/DetalleCursoUsuario/DetalleCursoUsuario.jsx
@@ -5,12 +5,14 @@ import InfoGeneral from '/componentes/DetalleCurso/InfoGeneral/InfoGeneral.jsx';
 import BotonContinuar from '/componentes/DetalleCursoUsuario/BotonContinuar/BotonContinuar.jsx';
 import Acordeon from '/componentes/DetalleCurso/Acordeon/Acordeon.jsx';
 import Footer from '/componentes/Footer/Footer'
+{/*import Navbar from '/componentes/Navbar/components/Container'*/}
 
 
 const DetalleCursoUsuario = () => {
 
     return (
         <>
+          {/*<Navbar />*/}
             <div className="top-row">
                 <InfoGeneral />
                 <BotonContinuar/>
